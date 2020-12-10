51af20a606f42c28566d4b4f2e6be69a3e208d3f
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2020-12-10 18:38:02 +0000

    親子コンポーネント間でのデータの受け渡しに関する講義を終了

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>いいね({{ halfNumber }})</p>
+    <p>{{ testProps }}</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["totalNumber", "testProps"],
+  // data: function() {
+  //   return {
+  //     number: 5
+  //   };
+  // },
+  computed: {
+    halfNumber() {
+      return this.totalNumber / 2
+    }
+  },
+  methods: {
+    increment() {
+      this.$emit("my-click", this.totalNumber + 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
