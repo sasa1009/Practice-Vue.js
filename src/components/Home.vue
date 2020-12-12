13a34dcce08c30aaafa3eafde990a8a5232297c8
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2020-12-12 16:17:44 +0000

    カスタムディレクティブに関する講義を修了

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,18 @@
 <template>
-  <p>Home</p>
+  <p v-border:solid.round="{width: '5px', color: 'yellow'}">Home</p>
 </template>
+
+<script>
+export default {
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+    } 
+  }
+}
+</script>
